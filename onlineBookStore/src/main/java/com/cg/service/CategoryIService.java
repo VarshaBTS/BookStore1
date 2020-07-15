f6ac0398dbc3e46ae586518f2bd06d03d0382019
varsha <vmusham@gmail.com>
2020-07-15 17:15:03 +0530

    1st commit after connecting

---
+++ onlineBookStore/src/main/java/com/cg/service/CategoryIService.java
@@ -0,0 +1,10 @@
+package com.cg.service;
+
+import com.cg.entity.Book;
+import com.cg.entity.Category;
+public interface CategoryIService {
+	Category addCategory (Category category);
+	Book addBook (Book book);
+	Book btoC(int cid,int bid);
+
+}
---
+++ onlineBookStore/src/main/java/com/cg/service/CategoryService.java
@@ -0,0 +1,45 @@
+package com.cg.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.dao.BookyDao;
+import com.cg.dao.CategoryDao;
+import com.cg.entity.Book;
+import com.cg.entity.Category;
+
+@Service
+@Transactional
+public class CategoryService implements CategoryIService {
+	@Autowired
+	private CategoryDao dao;
+	
+	@Autowired
+	private BookyDao dao1;
+	
+	public Category addCategory(Category category) {
+	return dao.save(category);
+	}
+
+	@Override
+	public Book addBook(Book book) {
+		return dao1.save(book);
+	}
+
+	@Override
+	public Book btoC(int cid, int bid) {
+		// TODO Auto-generated method stub
+		Book b=dao1.getOne(bid);
+		if(b!=null) {
+			Category c=dao.getOne(cid);
+			b.setCategory(c);
+		}
+		return dao1.save(b);
+	}
+	
+
+
+
+}
